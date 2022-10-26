dd8afdab9f75b2e72ee61f097b4e6edc7e18ba2f
saikatnoman12 <noommaa240@gmail.com>
2022-10-27 02:31:27 +0600

    add sun and moon color

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const sideMenu = document.querySelector('aside');
 const menuBtn = document.querySelector('#menu-btn');
 const closeBtn = document.querySelector('#close-btn');
+const themeToggle = document.querySelector('.theme-toggle');
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
@@ -9,3 +10,7 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 });
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+})
