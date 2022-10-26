da88e1917aa238d8ca42645c76dc65fe8de8f3cb
saikatnoman12 <noommaa240@gmail.com>
2022-10-27 03:16:31 +0600

    complete this dashboard design

--- js/main.js
+++ js/main.js
@@ -1,16 +1,66 @@
-const sideMenu = document.querySelector('aside');
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
-const themeToggle = document.querySelector('.theme-toggle');
 
-menuBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'block';
-});
+(function () {
 
-closeBtn.addEventListener('click', () => {
-    sideMenu.style.display = 'none';
-});
+    const sideMenu = document.querySelector('aside');
+    const menuBtn = document.querySelector('#menu-btn');
+    const closeBtn = document.querySelector('#close-btn');
+    const themeToggle = document.querySelector('.theme-toggle');
 
-themeToggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme-variables');
-})
+    menuBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'block';
+    });
+
+    closeBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'none';
+    });
+
+    themeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme-variables');
+
+        themeToggle.firstElementChild.classList.toggle('active');
+        themeToggle.lastElementChild.classList.toggle('active');
+
+    });
+
+})();
+
+
+(function () {
+
+    const table = document.querySelector('.recent-orders table tbody');
+
+    orders.forEach(order => {
+        const tr = document.createElement('tr');
+
+        for (let orderEle in order) {
+
+            const td = document.createElement('td');
+            td.innerHTML = order[orderEle];
+            tr.appendChild(td);
+
+        }
+
+        table.appendChild(tr);
+
+    });
+
+
+})();
+
+
+(function () {
+
+    const sideVarItems = document.querySelectorAll('.sidebar-item');
+
+    sideVarItems.forEach((sideBar) => {
+        sideBar.addEventListener('click', () => {
+
+            const current = document.querySelector('.active');
+            current.classList.remove('active');
+
+            sideBar.classList.add('active');
+
+        });
+    });
+
+})();
