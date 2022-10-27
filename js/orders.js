77f2eedef8292c41b14a06dba708c3263bca4635
saikatnoman12 <noommaa240@gmail.com>
2022-10-27 14:37:39 +0600

    modify dashboard

--- js/orders.js
+++ js/orders.js
@@ -40,5 +40,47 @@ const orders = [
         productNumber: '96832',
         paymentStatus: 'Due',
         shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '5455',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Pro Mini Drone',
+        productNumber: '2395',
+        paymentStatus: 'Refunded',
+        shipping: 'Decliend'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '3455',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Bongobondu Drone',
+        productNumber: '5554',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Mini Drone',
+        productNumber: '2935',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Drone',
+        productNumber: '56564',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Pro Mini Drone',
+        productNumber: '96832',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
     }
 ];
